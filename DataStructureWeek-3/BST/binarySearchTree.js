31ec99e82ea6f9dbe793ced9f5486d857bfd80fd
meathirarose <athirarosejohn@gmail.com>
2024-06-25 12:44:06 +0530

    BST - additional functions

--- DataStructureWeek-3/BST/binarySearchTree.js
+++ DataStructureWeek-3/BST/binarySearchTree.js
@@ -33,11 +33,66 @@ class BinarySearchTree {
             }
         }else{
             if(root.right === null){
-
+                root.right = newNode;
+            }else{
+                this.insertNode(root.right, newNode);
             }
         }
     }
+
+    search(root, value){
+        if (!root) {
+            return false;
+        }else{
+            if (root.value === value) {
+                return true;
+            }else if (value < root.value){
+                return this.search(root.left, value);
+            }else{
+                return this.search(root.right, value);
+            }
+        }
+    }
+
+    preOrder(root){
+        if (root) {
+            console.log(root.value);
+            this.preOrder(root.left);
+            this.preOrder(root.right);
+        }
+    }
+
+    inOrder(root){
+        if(root){
+            this.inOrder(root.left);
+            console.log(root.value);
+            this.inOrder(root.right);
+        }
+    }
+
+    postOrder(root){
+        if(root){
+            this.postOrder(root.left);
+            this.postOrder(root.right);
+            console.log(root.value);
+        }
+    }
+
 }
 
 const bst = new BinarySearchTree();
 console.log(bst.isEmpty());
+bst.insert(10);
+bst.insert(5);
+bst.insert(15);
+bst.insert(3);
+bst.insert(7);
+console.log(bst.isEmpty());
+console.log(bst.search(bst.root, 10));
+console.log(bst.search(bst.root, 19));
+console.log("Pre Order");
+bst.preOrder(bst.root);
+console.log("In Order");
+bst.inOrder(bst.root);
+console.log("Post Order");
+bst.postOrder(bst.root);
