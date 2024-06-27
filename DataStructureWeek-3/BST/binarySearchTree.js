ba3af70be310f375cc9d91941a1fdaa56f32bf6f
meathirarose <athirarosejohn@gmail.com>
2024-06-27 14:08:40 +0530

    BST - find closest value

--- DataStructureWeek-3/BST/binarySearchTree.js
+++ DataStructureWeek-3/BST/binarySearchTree.js
@@ -93,6 +93,68 @@ class BinarySearchTree {
         }
     }
 
+    minValue(root){
+        if (!root.left) {
+            return root.value
+        }else{
+            return this.minValue(root.left)
+        }
+    }
+
+    maxValue(root){
+        if(!root.right){
+            return root.value;
+        }else{
+            return this.maxValue(root.right);
+        }
+    }
+
+    delete(value){
+        this.root = this.deleteNode(this.root, value);
+    }
+
+    deleteNode(root, value){
+        if (root === null) {
+            return root;
+        }
+        if(value < root.value){
+            root.left = this.deleteNode(root.left, value);
+        }else if(value > root.value){
+            root.right = this.deleteNode(root.right, value);
+        }else{
+            if(!root.left && !root.right){
+                return null;
+            }
+            if(!root.left){
+                return root.right;
+            }else if(!root.right){
+                return root.left;
+            }
+            root.value = this.minValue(root.right);
+            root.right = this.deleteNode(root.right, root.value)
+        }
+        return root;
+    }
+
+    closestValue(target){
+        return this.findClosestValue(this.root, target, this.root.value);
+    }
+
+    findClosestValue(root, target, closest){
+        if(root === null)
+            return closest;
+        if(Math.abs(target - closest) > Math.abs(target - root.value)){
+            closest = root.value;
+        }
+        if(target < root.value){
+            return this.findClosestValue(root.left, target, closest);
+        }else if(target > root.value){
+            return this.findClosestValue(root.right, target, closest);
+        }else{
+            return closest;
+        }
+    }
+
 }
 
 const bst = new BinarySearchTree();
@@ -113,3 +175,7 @@ console.log("Post Order");
 bst.postOrder(bst.root);
 console.log("Breadth first search");
 bst.levelOrder();
+console.log("Minimum value = ", bst.minValue(bst.root));
+console.log("Maximum value = ", bst.maxValue(bst.root));
+
+console.log(`Closest value to ${13} = `, bst.closestValue(13));
