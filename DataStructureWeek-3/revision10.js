615410984167ff0d18f328044d005df1af0845c6
meathirarose <athirarosejohn@gmail.com>
2024-07-12 17:08:18 +0530

    Revision

--- DataStructureWeek-3/revision10.js
+++ DataStructureWeek-3/revision10.js
@@ -1,63 +1,75 @@
-class Node {
-    constructor() {
-        this.children = {};
-        this.isEnd = false;
+function buildMinHeap(arr) {
+    let n = arr.length;
+    for (let i = Math.floor((n-1)/2); i>=0; i--){
+        heapify(arr, n, i);
+    }
+    return arr;
+}
+function heapify(arr, n, i) {
+    let left = 2*i+1;
+    let right = 2*i+2;
+    let smallest = i;
+
+    if (left < n && arr[left] < arr[smallest]) {
+        smallest = left;
+    }
+    if (right < n && arr[right] < arr[smallest]) {
+        smallest = right;
+    }
+    if (smallest !== i) {
+        [arr[i], arr[smallest]] = [arr[smallest], arr[i]];
+        heapify(arr, n, smallest);
     }
 }
-class Trie {
-    constructor() {
-        this.root = new Node();
-    }
-    insert(str){
-        let node = this.root;
-        for (let i = str.length-1; i>=0; i--) {
-            let char = str[i]
-            if (!node.children[char]) {
-                node.children[char] = new Node();
-            }
-            node = node.children[char];
+function insert(arr, key) {
+    arr.push(key);
+    let i = arr.length - 1;
+
+    while (i>0) {
+        let root = Math.floor((i-1)/2);
+        if (arr[root] <= arr[i]) {
+            break;
         }
-        node.isEnd = true;
-    }
-    // search(prefix){
-    //     let node = this.root;
-    //     for (const char of prefix) {
-    //         if (!node.children[char]) {
-    //             return false;
-    //         }
-    //         node = node.children[char];
-    //     }
-    //     return node.isEnd;
-    // }
-    searchWithSuffix(suffix){
-        let result = [];
-        let node = this.root;
-        for (let i = suffix.length-1; i>=0; i--){
-            let char = suffix[i];
-            if (!node.children[char]) {
-                return [];
-            }
-            node = node.children[char];
-        }
-        this.collectWords(node, result, suffix);
-        return result;
-    }
-    collectWords(node, result, suffix){
-        if (node.isEnd) {
-            return result.push(suffix);
-        }else{
-            for (const char in node.children) {
-                this.collectWords(node.children[char], result, char+suffix)
-            }
-        }
+        [arr[i], arr[root]] = [arr[root], arr[i]];
+        i = root;
     }
 }
 
-const trie = new Trie();
-trie.insert('apple');
-trie.insert('orange');
-trie.insert('pineapple');
-trie.insert('watermelon');
-trie.insert('melon');
-console.log(trie);
-console.log("is 'ple' is present in the trie? ", trie.searchWithSuffix('ple'));
+function deleteRoot(arr) {
+    let n = arr.length;
+    if (n === 0) {
+        return null;
+    }
+
+    let root = arr[0];
+    arr[0] = arr[n-1];
+    arr.pop();
+    n--;
+
+    heapify(arr, n, 0);
+    return root;
+}
+
+function heapSort(arr) {
+    let n = arr.length;
+
+    buildMinHeap(arr);
+
+    for (let i = n-1; i>0; i--){
+        [arr[0], arr[i]] = [arr[i], arr[0]];
+        heapify(arr, i, 0);
+    }
+    return arr;
+}
+
+const arr = [65, 34, 23, 45, 12, 67];
+console.log(buildMinHeap(arr));
+insert(arr, 10);
+console.log(buildMinHeap(arr));
+deleteRoot(arr);
+console.log(buildMinHeap(arr));
+heapSort(arr);
+console.log(buildMinHeap(arr));
+
+
+
